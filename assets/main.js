18eeeaa0bcf63671ee72ad1be77d38b6fe6f8a72
Ale <alessandrochb@gmail.com>
2020-12-01 16:23:49 +0100

    ricreato header dinamicamente

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+var app = new Vue ({
+    el: '#root',
+    data: {
+        menuVoices: {
+                home: '#',
+                mission: '#',
+                causes: '#',
+                journal: 'https://www.google.com',
+            },
+        headerButton: 'donate',
+        logoHeader: 'avada-charity-logo.png',
+    },
+
+})
