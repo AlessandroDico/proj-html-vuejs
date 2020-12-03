3e8288c67442b897de9cdee8fe3738992b059df5
Ale <alessandrochb@gmail.com>
2020-12-03 12:08:25 +0100

    cambiato colore header on scroll

--- assets/main.js
+++ assets/main.js
@@ -55,6 +55,30 @@ var app = new Vue ({
         ],
         headerButton: 'donate',
         logoHeader: 'avada-charity-logo.png',
+        logoHeaderMobile: 'avada-charity-mobile-logo-retina.png',
+        headerRightClass: 'header-right',
+        changeHeader: null
     },
+    methods: {
+    // creo una funzione che allo scroll cambia le classi all header e all header right
+    // quando lo scroll nella 'window' è maggiore di 20px (window.scrollY)
+    //quando la larghezza della window è sopra si 972px (window.innerWidth)
+        changeOnScroll(){
+            if (this.headerRightClass = 'header-right' && window.scrollY > 20 && window.innerWidth > 972) {
+                this.headerRightClass = 'header-right-scroll';
+                this.changeHeader = 'header-scroll';
+                this.logoHeader = this.logoHeaderMobile;
+            } else {
+                this.headerRightClass = 'header-right';
+                this.changeHeader = 'null';
+                this.logoHeader = 'avada-charity-logo.png';
+            }
+        }
+
+    },
+    mounted(){
+        window.addEventListener('scroll', this.changeOnScroll);
+    },
+
 
 })
